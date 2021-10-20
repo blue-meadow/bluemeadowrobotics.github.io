a040f30ee77fc72d06186acd2dedf5b417e5de3b
Milo Knowles <mknowles@mit.edu>
2021-10-20 17:45:56 -0400

    WIP

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -4,41 +4,6 @@ import { Row, Col, Card } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
 
-class TeamCardIzzy extends Component {
-  render() {
-    return (
-      <Row className="pt-3 pb-3">
-        <Col xs={0} md={2}></Col>
-        <Col xs={12} md={3} className="my-auto">
-          <div className="">
-            <img className="img-fluid pb-1" variant="top" src="images/team/izzy-square.jpg" style={{padding: 0}}/>
-          </div>
-        </Col>
-        <Col xs={12} md={6} className="my-auto">
-          <Card className="card-borderless" border="">
-          <Card.Body>
-            <Card.Title className="bm-blue-color team-card-title">Isaiah Brand</Card.Title>
-            <Card.Subtitle className="text-muted pb-3 monospace-font">CTO & Co-Founder</Card.Subtitle>
-            <p className="team-card-text">
-              Izzy completed a B.S in Applied Math and Computer Science at Brown, and
-              a Masters in AI at MIT. He's worked on planning & controls software and vehicle hardware
-              at Microsoft Research, Skydio, and Greensight Agronomics.
-            </p>
-            {/*where his research focused on vision, reinforcement learning, and motion planning.*/}
-            <Card.Link href="izzy@bluemeadow.ai">izzy@bluemeadow.ai</Card.Link>
-            <Card.Link href="https://www.linkedin.com/in/izzybrand/"><span className="fa fa-linkedin"></span></Card.Link>
-            {/* <Card.Link href="https://github.com/IzzyBrand"><span className="fa fa-github"></span></Card.Link> */}
-            {/* <Card.Link href="https://izzybrand.com/">Website</Card.Link> */}
-          </Card.Body>
-        </Card>
-        </Col>
-        <Col xs={0} md={1}></Col>
-      </Row>
-    );
-  }
-};
-
-
 class TeamSection extends Component {
   render() {
     return (
--- src/components/TechnologySection.jsx
+++ src/components/TechnologySection.jsx
@@ -12,23 +12,22 @@ class TechnologySection extends Component {
         <Container className="pt-5 pb-5">
           <Row>
             <Col>
-              {/* <p className="section-header-text monospace-font bm-blue-color AnimateOnScroll animate__animated animate__fadeIn">TECHNOLOGY</p> */}
               <p className="section-standout-text">
-                Blue Meadow is developing a platform for data-driven ocean farming.
+                Blue Meadow is developing a platform for data-driven, regenerative ocean farming.
               </p>
             </Col>
           </Row>
           <Row className="mt-5">
             <Col>
               <p className="section-header-text monospace-font bm-blue-color AnimateOnScroll animate__animated animate__fadeIn">
-                DATA COLLECTION ROBOT
+                HARDWARE PLATFORM
               </p>
             </Col>
           </Row>
           <Row className="pb-3">
             <Col>
               <p className="body-text">
-                Our data collection robot monitors water conditions throughout the growing season.
+                Our low-cost, autonomous data collection robot monitors water conditions throughout the growing season.
               </p>
             </Col>
           </Row>
@@ -50,43 +49,44 @@ class TechnologySection extends Component {
             <Col xs={12} md={5} className="my-auto">
               <p className="monospace-font section-header-text bm-blue-color">VERTICAL PROFILING</p>
               <p className="body-text">
-                The robot profiles the entire water column using its tethered sensor package.
+                The robot collects data throughout the water column using a tethered sensor package.
               </p>
             </Col>
           </Row>
           <Row className="pt-5">
             <Col>
               <p className="section-header-text monospace-font bm-blue-color AnimateOnScroll animate__animated animate__fadeIn">
-                FARMER DASHBOARD
+                SOFTWARE PLATFORM
               </p>
             </Col>
           </Row>
           <Row className="pb-3">
             <Col>
               <p className="body-text">
-                Blue Meadow's farmer dashboard translates raw data into actionable insights.
+                Blue Meadow's farmer dashboard visualizes and translates raw data into actionable insights.
               </p>
             </Col>
           </Row>
           <Row className="">
             <Col>
-              <img className="img-fluid" src="images/farmview.png"></img>
+              {/* <img className="img-fluid" src="images/farmview.png"></img> */}
+              <img className="img-fluid" src="images/farmview2.png"></img>
             </Col>
           </Row>
           <Row>
             <Col xs={12} md={4} className="">
               <Card className="card-borderless">
-                <p className="monospace-font section-header-text bm-blue-color pt-5">PREDICT AND PREVENT LOSSES</p>
+                <p className="monospace-font section-header-text bm-blue-color pt-5">PREVENT LOSSES</p>
                 <p className="body-text">
-                  The dashboard notifies farmers when a potential disease, fouling, or mortality risk is detected.
+                  Receive early warnings for disease, fouling, and other environmental risks.
                 </p>
               </Card>
             </Col>
             <Col xs={12} md={4} className="">
               <Card className="card-borderless">
-                <p className="monospace-font section-header-text bm-blue-color pt-5">IMPROVE CROP YIELDS</p>
+                <p className="monospace-font section-header-text bm-blue-color pt-5">PREDICT & IMPROVE YIELDS</p>
                 <p className="body-text">
-                  Determine the optimal positioning of gear in the water column.
+                  Predict crop growth based on environmental factors at the farm site. Determine the optimal positioning of gear to maximize growth rate.
                 </p>
               </Card>
             </Col>
@@ -94,7 +94,7 @@ class TechnologySection extends Component {
               <Card className="card-borderless">
                 <p className="monospace-font section-header-text bm-blue-color pt-5">PLAN TASKS</p>
                 <p className="body-text">
-                  Plan seeding, harvesting, and other tasks based on current conditions.
+                  Plan seeding and harvesting times based on current and projected water conditions.
                 </p>
               </Card>
             </Col>
